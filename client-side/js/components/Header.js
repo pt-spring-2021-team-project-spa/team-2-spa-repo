04a9f415fdeda5bba282e5a756e833776a6717ef
shuaybfarah <shuaybfarah18@gmail.com>
2021-07-11 15:06:44 -0400

    created nav bar and folder stuc

---
+++ client-side/js/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+    return `
+        <ul class='nav__list'>
+            <li class='nav__list_home'>Home</li>
+            <li class='nav__list_art'>Art</li>
+            <li class='nav__list_science'>Science</li>
+            <li class='nav__list_history'>History</li>
+            <li class='nav__list_about'>About Us</li>
+            <li class='nav__list_contact'>Contact Us</li>
+        </ul>
+    `;
+}
