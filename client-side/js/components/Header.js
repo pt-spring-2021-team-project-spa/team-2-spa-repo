c5fa20b10576b00f2178e32dfc4dc526d18a754d
shuaybfarah <shuaybfarah18@gmail.com>
2021-07-11 16:44:12 -0400

    finished nav buttons, demoed and approved

--- client-side/js/components/Header.js
+++ client-side/js/components/Header.js
@@ -2,11 +2,11 @@ export default function Header() {
     return `
         <ul class='nav__list'>
             <li class='nav__list_home'>Home</li>
-            <li class='nav__list_art'>Art</li>
+            <li class='nav__list_artList'>Art</li>
             <li class='nav__list_science'>Science</li>
             <li class='nav__list_history'>History</li>
-            <li class='nav__list_about'>About Us</li>
-            <li class='nav__list_contact'>Contact Us</li>
+            <li class='nav__list_aboutUs'>About Us</li>
+            <li class='nav__list_contactUs'>Contact Us</li>
         </ul>
     `;
 }
--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,17 +1,68 @@
 import Header from "./components/Header.js";
-
-
-
-// let app = document.querySelector('#app');
+// import HomePage from "./pages/HomePage.js"
+// import ArtListPage from "./pages/ArtListPage.js"
+// import SciencePage from "./pages/SciencePage.js"
+// import HistoryPage from "./pages/HistoryPage.js"
+// import AboutUsPage from "./pages/AboutUsPage.js"
+// import ContactUsPage from "./pages/ContactUsPage.js"
 
 buildPage();
 
 function buildPage(){
     header();
+    navigateToHomePage();
+    navigateToArtListPage();
+    navigateToSciencePage();
+    navigateToHistoryPage();
+    navigateToAboutUsPage();
+    navigateToContactPage();
 }
+
+const app = document.querySelector('#app');
 
 function header(){
     const headerElement = document.querySelector('.header');
     headerElement.innerHTML = Header();
 }
 
+function navigateToHomePage() {
+    const homeButton = document.querySelector('.nav__list_home');
+    homeButton.addEventListener('click', () => {
+        app.innerHTML = HomePage();
+    })
+}
+
+function navigateToArtListPage() {
+    const artButton = document.querySelector('.nav__list_artList');
+    artButton.addEventListener('click', () => {
+        app.innerHTML = ArtListPage();
+    })
+}
+
+function navigateToSciencePage() {
+    const scienceButton = document.querySelector('.nav__list_science');
+    scienceButton.addEventListener('click', () => {
+        app.innerHTML = SciencePage();
+    })
+}
+
+function navigateToHistoryPage() {
+    const historyButton = document.querySelector('.nav__list_history');
+    historyButton.addEventListener("click", () => {
+        app.innerHTML = HistoryPage();
+    })
+}
+
+function navigateToAboutUsPage() {
+    const aboutUsButton = document.querySelector('.nav__list_aboutUs');
+    aboutUsButton.addEventListener('click', () => {
+        app.innerHTML = AboutUsPage();
+    })
+}
+
+function navigateToContactPage() {
+    const contactPageButton = document.querySelector('.nav__list_contactUs');
+    contactPageButton.addEventListener('click', () => {
+        app.innerHTML = ContactUsPage();
+    })
+}
