400d6b09c56fc0eb9914aefbbe1e324feb77979b
alindley0729 <lindley0729@gmail.com>
2021-07-13 17:55:15 -0400

    addingaboutuspage

---
+++ src/main/resources/AboutUs.js
@@ -0,0 +1,75 @@
+export default function AboutUs() {
+    return `
+      <h1>About Our Team</h1>
+      <h3>Here is a little bit more about our team</h3>
+</div>
+  <div class="employees">
+    <div class="column">
+      <div class="card">
+        <div class="container">
+          <h2>Ian Ford</h2>
+          <p class="title">Director</p>
+          <p1> Desription here</p1>
+          <p class="email">IFord@spa.org</p>
+          <button type="Contact">Contact</button>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="card">
+        <div class="container">
+          <h2>Lee Powell</h2>
+          <p class="title">Director</p>
+          <p1> Decription here</p1>
+          <p class="email">LPowell@Spa.org</p>
+          <button type="Contact">Contact</button>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="card">
+        <div class="container">
+          <h2>Nick Billhardt</h2>
+          <p class="title">Director</p>
+          <p1> Description here</p1>
+          <p class="email">NBillhardt@Spa.org</p>
+          <button type="Contact">Contact</button>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="card">
+        <div class="container">
+          <h2>Tyler Koenig</h2>
+          <p class="title">Director</p>
+          <p1> Description here</p1>
+          <p class="email">TKoenig@Spa.org</p>
+          <button type="Contact">Contact</button>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+        <div class="card">
+          <div class="container">
+            <h2>Shuayb Farrah</h2>
+            <p class="title">Director</p>
+            <p1> Decription here</p1>
+            <p class="email">SFarrah@Spa.org</p>
+            <button type="Contact">Contact</button>
+          </div>
+        </div>
+      </div>
+    <div class="column">
+      <div class="card">
+        <div class="container">
+          <h2>Amber Lindley</h2>
+          <p class="title"></p>
+          <p1> Add text that describes person</p1>
+          <p class="email">Alindley@Spa.org</p>
+          <button type="Contact">Contact</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+}
