5516a875e403e9e4a8037d7e4aa46f10dffed398
leepowe <leepowell2@yahoo.com>
2021-07-16 00:14:30 -0400

    redid lost commit something I saw on youtube

--- client-side/js/pages/SciencePage.js
+++ client-side/js/pages/SciencePage.js
@@ -1,17 +1,28 @@
-export default function Images(images) {
-  console.log(images);
-  const items = images.collection.itmes;
-  console.log(items);
-  return `
-    <h1>Science</h1>
-    ${items
-      .map((item) => {
-        console.log(item.links);
-        const itemImage = item.links[0].href;
-        return `
-                <img src=${itemImage} alt='' />
-            `;
-      })
-      .join("")}
-    `;
+// export default function BlindedWithScience() {
+//   return `
+//     <h1>Science</h1>
+//     ${items
+//       .map((item) => {
+//         console.log(item.links);
+//         const itemImage = item.links[0].href;
+//         return `
+//                 <img src=${itemImage} alt='' />
+//             `;
+//       })
+//       .join("")}
+//     `;
+// }
+
+export default async function SciencePage() {
+  let API_KEY = "WV0eX3Rt3FuYTS6kbJpJ5S5VPlEgCVqgf13mD7NM";
+  let response = await fetch(
+    "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=${API_KEY}"
+  );
+  let data = await response.json();
+  useApiData(data);
+}
+
+function useApiData(data) {
+  document.querySelector("#content").innerHTML += data.name;
+  document.querySelector("#content").innerHTML += `<img src="${data.img_src}">`;
 }
