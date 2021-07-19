834877d55d1cbcfad7a533b06916bd946d013698
Tyler Koenig <tylerkng96@icloud.com>
2021-07-18 20:42:27 -0400

    style added to Art Page

--- client-side/js/components/Art.js
+++ client-side/js/components/Art.js
@@ -2,8 +2,8 @@ import '../../css/ArtStyle.css';
 
 export default function Art() {
 	return `
-    <h2>Art Categories</h2>
-        <ul class='art__list'>
+    <ul class='art__list'>
+    <h2>ART CATEGORIES!</h2>
             <li class='art__list_asianArtsList'>Asian Arts</li>
             <li class="art__list_paintingsList">European Paintings</li>
             <li class='art__list_printsList'>Drawings and Prints</li>
