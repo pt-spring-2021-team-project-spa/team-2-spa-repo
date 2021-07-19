127f61bc983ef7061e108cd2a53da6c21c251ad1
shuaybfarah <shuaybfarah18@gmail.com>
2021-07-19 08:31:12 -0400

    add styling to history page

--- client-side/js/components/WorldWonderTile.js
+++ client-side/js/components/WorldWonderTile.js
@@ -1,8 +1,11 @@
 export default function WorldWonderTile(worldWonder) {
     return `
         <div class="worldWonder__tile_container">
+        <div class="test">
+        <div class="content-wrapper">
             <input type="hidden" id="worldWonderId" value=${worldWonder.url} />
             <h2 class="worldWonder__name">${worldWonder.name}</h2>
+            <img class="thumb" style=" 'background-image: url(' + ${worldWonder.imgUrl} + ');'">
             <p>Location: ${worldWonder.location}</p>
             <p>Year Of Construction: ${worldWonder.yearOfConstruction}</p>
             <p>Height: ${worldWonder.height}</p>
@@ -10,6 +13,8 @@ export default function WorldWonderTile(worldWonder) {
             <p>Age: ${worldWonder.age}</p>
             <p>Fun Fact: ${worldWonder.funFact}</p>
             <p>Myth: ${worldWonder.myth}</p>
+            </div>
+            </div>
         </div>
     `;
 }
